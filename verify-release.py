66670bc44905a255ea1f5d6fb0f52a5a2675e603
Chip Childers <chipchilders@apache.org>
2013-04-22 21:32:04 -0400

    Updates

--- verify-release.py
+++ verify-release.py
@@ -12,8 +12,19 @@ class ReleaseVerifier:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
+    def __init__(self, input_commands, version, commitsh):
+        self.input_commands = input_commands
+        self.version = version
+        self.commitsh = commitsh
+
+    def do_replacements(self, command):
+        command = command.replace("${commit-sh}", self.commitsh)
+        command = command.replace("${version}", self.version)
+        return command
+
     def make_call(self, action):
         output = ""
+        action["command"] = self.do_replacements(action["command"])
         try:
             print "COMMAND: " + action["command"]
             if "cwd" in action:
@@ -37,8 +48,8 @@ class ReleaseVerifier:
                 self.results.append(action)
                 print "OUTPUT: " + output
 
-    def make_calls(self, input_commands):
-        for action in input_commands:
+    def make_calls(self):
+        for action in self.input_commands:
             self.make_call(action)
 
     def print_results(self):
@@ -52,10 +63,12 @@ class ReleaseVerifier:
 
 def main(argv):
     inputfile = ''
+    version = ''
+    commitsh = ''
     try:
-        opts, args = getopt.getopt(argv,"hi:",["ifile="])
+        opts, args = getopt.getopt(argv,"hi:c:v:")
     except getopt.GetoptError:
-        print 'verify-release.py -i <inputfile> '
+        print 'verify-release.py -i <inputfile> -c <commit-sh> -v <version>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -63,13 +76,17 @@ def main(argv):
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
+        elif opt in ("-c"):
+            commitsh = arg
+        elif opt in ("-v"):
+            version = arg
 
     myfile = open(inputfile, "r")
     inputfiletext=myfile.read()
 
     input_commands = ast.literal_eval(inputfiletext)
-    x = ReleaseVerifier()
-    x.make_calls(input_commands)
+    x = ReleaseVerifier(input_commands, version, commitsh)
+    x.make_calls()
     x.print_results()
 
 if __name__ == "__main__":
